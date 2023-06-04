f502131609e33fa57b54e22eb68d4b5927dc6e85
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2023-06-04 13:22:48 +0530

    Added Complete Responsiveness

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const nav = document.getElementById('navbar');
 
 if(bar) {
     bar.addEventListener('click', () => {
-        nav.classList.add('actve');
+        nav.classList.add('active');
     })
 }
 
 if(close) {
     close.addEventListener('click', () => {
-        nav.classList.remove('actve');
+        nav.classList.remove('active');
     })
 }
 
